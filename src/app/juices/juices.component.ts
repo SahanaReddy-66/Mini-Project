761aab6441045b763cb64ddb170b0b0003a42694
Sahana Reddy <sahanacmreddy@gmail.com>
2022-05-09 01:02:22 +0530

    Mini Project done as per Requirment

---
+++ src/app/juices/juices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juices',
+  templateUrl: './juices.component.html',
+  styleUrls: ['./juices.component.scss']
+})
+export class JuicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
